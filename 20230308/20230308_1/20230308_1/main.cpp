941b1737e0aca1ba7f2833d91d40d422bb74e5e3
서형준 <125755630+jun0404038@users.noreply.github.com>
2023-03-08 09:49:35 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>  // header 파일 추가 standard IO
+
+
+int main(void)
+{
+	printf("test\n"); // \n = line feed 한 줄 띄우기
+
+	printf("202315008\n"); // \n = line feed 한 줄 띄우기
+
+	printf("Hyeong Jun SEO\n"); // \n = line feed 한 줄 띄우기
+	return 0;
+}
+
+// ctrl+f5 는 실행
