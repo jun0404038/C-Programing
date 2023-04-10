00607800250b3d05485f312734e865bd8276da49
서형준 <125755630+jun0404038@users.noreply.github.com>
2023-04-10 21:58:42 +0900

    Add files via upload

---
+++ 20230410/20230410-1/20230410-1/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(void)
+{
+	int x = 1, y = 2, result;
+	result = x + y;
+
+	printf("x에 y를 더한 값은 %d입니다.", result);
+}
---
+++ 20230410/20230410-2/20230410-2/main.cpp
@@ -0,0 +1,13 @@
+#define PI 3.141592
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 1;
+	float b = PI;
+
+	printf("µ¡¼À: ");
+	printf("a + b = %f\n", a + b);
+
+
+}
